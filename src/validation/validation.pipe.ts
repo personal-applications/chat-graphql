e8c4ff907e7245ec32521352b817a98df003efb7
Phạm Thành Trung <adamward459@gmail.com>
2023-12-24 00:54:49 +0700

    Add ValidationPipe for input validation

---
+++ src/validation/validation.pipe.ts
@@ -0,0 +1,36 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { GraphQLError } from 'graphql';
+import { StatusCodes } from 'http-status-codes';
+
+@Injectable()
+export class ValidationPipe implements PipeTransform<any> {
+  async transform(value: any, { metatype }: ArgumentMetadata) {
+    if (!metatype || !this.toValidate(metatype)) {
+      return value;
+    }
+    const object = plainToInstance(metatype, value);
+    const errors = await validate(object);
+    if (errors.length > 0) {
+      const errorMessage = Object.values(errors[0].constraints)[0];
+
+      throw new GraphQLError(errorMessage, {
+        extensions: {
+          code: StatusCodes.BAD_REQUEST,
+          http: {
+            status: StatusCodes.BAD_REQUEST,
+          },
+        },
+      });
+    }
+    return value;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  private toValidate(metatype: Function): boolean {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    const types: Function[] = [String, Boolean, Number, Array, Object];
+    return !types.includes(metatype);
+  }
+}
